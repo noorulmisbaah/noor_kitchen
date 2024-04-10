b8c329d955cd41b14a50d1f636ae38c253f81383
noorulmisbaah <noorulmisbaah.noor@noor.com>
2024-04-10 12:26:37 +0100

    Managing Transactions v1.2.1

--- custom_modules/handlers/customer-handler.js
+++ custom_modules/handlers/customer-handler.js
@@ -1,20 +1,22 @@
 const util = require('./utils');
+const fs = require('fs');
 
 function renderCustomerPage(req, res) {
-    try {
-        const { username, email } = util.readJSONFile(`./users/${req.body.username}/user_information.json`);
-        const messages = util.reverseArrayContent(util.readJSONFile(`./users/${username}/messages.json`));
-        const allTransactions = util.reverseArrayContent(util.readJSONFile(`./users/${username}/transactions.json`));
-        var total = 0;
-    
-        for (var i = 0; i < allTransactions.length; i++) {
-            total += allTransactions[i].amount;
-        }
-    
-        res.render('customer-dashboard', { username, email, messages, allTransactions, total });
-    } catch (e) {
+    if (!(fs.existsSync(`./users/${req.body.username}`))) {
         res.redirect('/');
+        return;
     }
+    
+    const { username, email } = util.readJSONFile(`./users/${req.body.username}/user_information.json`);
+    const messages = util.reverseArrayContent(util.readJSONFile(`./users/${username}/messages.json`));
+    const allTransactions = util.reverseArrayContent(util.readJSONFile(`./users/${username}/transactions.json`));
+    var total = 0;
+
+    for (var i = 0; i < allTransactions.length; i++) {
+        total += allTransactions[i].amount;
+    }
+
+    res.render('customer-dashboard', { username, email, messages, allTransactions, total });
 }
 
 module.exports = { renderCustomerPage };
